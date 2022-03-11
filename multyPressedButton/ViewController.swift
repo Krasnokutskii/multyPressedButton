e3e29cced565c5dd100117338d43b64b666b234d
Krasnokutskii Iaroslav <Yroslavkr1995@gmail.com>
2022-03-12 00:25:16 +0300

    conflict

--- multyPressedButton/ViewController.swift
+++ multyPressedButton/ViewController.swift
@@ -45,12 +45,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func setGestures(){
         dueDateGesture = UITapGestureRecognizer(target: self, action: #selector(gesturePresssed(index:)))
         statusGesture = UITapGestureRecognizer(target: self, action: #selector(gesturePresssed(index:)))
-        dueDateGesture?.numberOfTapsRequired = 2
-        dueDateGesture?.numberOfTouchesRequired = 1
+        dueDateGesture?.numberOfTapsRequired = 222
+        dueDateGesture?.numberOfTouchesRequired = 111
        
         
-        statusGesture?.numberOfTapsRequired = 2
-        statusGesture?.numberOfTouchesRequired = 1
+        statusGesture?.numberOfTapsRequired = 222
+        statusGesture?.numberOfTouchesRequired = 111
         
         dueDatebutton?.addGestureRecognizer(dueDateGesture!)
         statusButton?.addGestureRecognizer(statusGesture!)
